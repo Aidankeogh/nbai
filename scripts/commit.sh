2ce067905e407a6f27ed1d664139659ae2e6017a
Aidan Keogh <aidankeogh@aidans-air.lan>
2021-04-01 16:47:57 -0700

    refactor

--- scripts/commit.sh
+++ scripts/commit.sh
@@ -3,16 +3,17 @@ python -m src.h5_db
 
 git add .
 
-if [ curr_branch="master" ]
+curr_branch=$(git rev-parse --abbrev-ref HEAD)
+if [[ curr_branch == "master" ]]
 then 
     echo "$curr_branch"
     git checkout -b $1
-    git commit -m "$1"
+    git commit --amend -m "$1"
     curr_branch=$1
-    git push origin HEAD
+    git push -u origin HEAD
 else
-    git commit -m "$curr_branch"
-    git push origin HEAD
+    git commit --amend -m "$curr_branch"
+    git push -u origin HEAD
 fi
 
 echo "https://github.com/Aidankeogh/nbai/compare/$curr_branch...master"

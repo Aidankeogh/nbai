9299a45ee9e514e57013b50f93a244de17ea1fcd
Aidan Keogh <aidankeogh@aidans-air.lan>
2021-04-01 17:19:49 -0700

    master

--- scripts/commit.sh
+++ scripts/commit.sh
@@ -10,10 +10,11 @@ then
     git checkout -b $1
     git commit -m "$1"
     curr_branch=$1
-    git push -u origin HEAD $2
+    curr_branch=$(git rev-parse --abbrev-ref HEAD)
+    #git push -u origin HEAD
 else
     git commit -m "$curr_branch"
-    git push -u origin HEAD $2
+    #git push -u origin HEAD $2
 fi
 
 echo "https://github.com/Aidankeogh/nbai/compare/master...$curr_branch"

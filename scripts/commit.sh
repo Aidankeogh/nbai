3b0db81b54c80ea6135a9cc39b3126f7e75b4662
Aidan Keogh <aidankeogh@gmail.com>
2021-04-01 17:24:53 -0700

    fix_commit_link

--- scripts/commit.sh
+++ scripts/commit.sh
@@ -7,17 +7,14 @@ curr_branch=$(git rev-parse --abbrev-ref HEAD)
 echo $curr_branch
 if [ "$curr_branch" == "master" ]
 then 
-    echo "$curr_branch"
-    echo "ON MASTER"
     git checkout -b $1
     git commit -m "$1"
     curr_branch=$1
     curr_branch=$(git rev-parse --abbrev-ref HEAD)
-    #git push -u origin HEAD
+    git push -u origin HEAD
 else
-
     git commit -m "$curr_branch"
-    #git push -u origin HEAD $2
+    git push -u origin HEAD $2
 fi
 
 echo "https://github.com/Aidankeogh/nbai/compare/master...$curr_branch"

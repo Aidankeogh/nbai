91f12ab82ad403d10cb92fc217f06368b15b81f3
Aidan Keogh <aidankeogh@aidans-air.lan>
2021-04-01 17:10:14 -0700

    thoughtpath

--- scripts/commit.sh
+++ scripts/commit.sh
@@ -1,15 +1,19 @@
 
+set -e
 python -m src.h5_db
-
 git add .
 
 curr_branch=$(git rev-parse --abbrev-ref HEAD)
-if [ curr_branch = "master" ]
+if [[ curr_branch == "master" ]]
 then 
+    echo "$curr_branch"
     git checkout -b $1
     git commit -m "$1"
-    git push origin HEAD
+    curr_branch=$1
+    git push -u origin HEAD
 else
     git commit -m "$curr_branch"
-    git push origin HEAD
+    git push -u origin HEAD
 fi
+
+echo "https://github.com/Aidankeogh/nbai/compare/$curr_branch...master"
